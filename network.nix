f91e10373dba3c484c027b862591a7543001db12
Almino Melo <almino.melo@gmail.com>
2023-12-03 20:43:37 -0300

    Remove coisas específicas da UFC

--- network.nix
+++ network.nix
@@ -10,19 +10,9 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  networking.hosts = {
-    "200.129.37.136" = [
-      "os.sobral.ufc.br"
-      "patrimonio.sobral.ufc.br"
-      "sobral.ufc.br"
-      "ti.sobral.ufc.br"
-    ];
-  };
-
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    # ports = [ 2398 ];
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = lib.mkDefault true;

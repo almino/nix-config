3a9405a7264813f404a20a55fc32b3c740edecad
Almino Melo <almino.melo@gmail.com>
2023-12-10 15:11:57 -0300

    Legibilidade

--- users.nix
+++ users.nix
@@ -4,9 +4,7 @@
   # services.accounts-daemon.enable = lib.mkDefault true;
 
   # https://nixos.org/manual/nixos/stable/options#opt-system.activationScripts
-  system.activationScripts = {
-    visitante = "passwd -d visitante";
-  };
+  system.activationScripts.visitante = "passwd -d visitante";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users =

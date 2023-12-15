8e3bc34ec54922fcd8258aaed8c7954afcd6cb67
Almino Melo <almino.melo@gmail.com>
2023-12-15 16:11:29 -0300

    O arquivo do GNOME no mesmo esquema do Cinnamon

--- cinnamon.nix
+++ cinnamon.nix
@@ -7,9 +7,6 @@
 {
   imports = [ ./default.nix ];
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Enable the Cinnamon Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.cinnamon.enable = true;
--- gnome.nix
+++ gnome.nix
@@ -1,6 +1,8 @@
 { lib, pkgs, ... }:
 
 {
+  imports = [ ./default.nix ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [

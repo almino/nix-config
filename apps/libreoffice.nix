9f3abf61f1870d45b949606ca7ffe769c92aeedb
Almino Melo <almino.melo@gmail.com>
2025-11-08 14:58:18 -0300

    Pra usar LibreOffice com Python

--- apps/libreoffice.nix
+++ apps/libreoffice.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 
+let
+  office = pkgs.libreoffice-fresh-unwrapped;
+in
 {
+  environment.sessionVariables = {
+    PYTHONPATH = "${office}/lib/libreoffice/program";
+    URE_BOOTSTRAP = "vnd.sun.star.pathname:${office}/lib/libreoffice/program/fundamentalrc";
+  };
+
   environment.systemPackages = with pkgs; [
     hunspell
     hunspellDicts.en_US

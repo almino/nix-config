d09e18667990a5730d3b8a1641f52dc9ed589756
Almino Melo <almino.melo@gmail.com>
2025-12-26 12:49:46 -0300

    Acentos não estavam funcionando!

--- default.nix
+++ default.nix
@@ -43,14 +43,25 @@
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
+    LC_COLLATE = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
     LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MESSAGES = "pt_BR.UTF-8";
     LC_MONETARY = "pt_BR.UTF-8";
     LC_NAME = "pt_BR.UTF-8";
     LC_NUMERIC = "pt_BR.UTF-8";
     LC_PAPER = "pt_BR.UTF-8";
     LC_TELEPHONE = "pt_BR.UTF-8";
     LC_TIME = "pt_BR.UTF-8";
+    LC_TYPE = "pt_BR.UTF-8";
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    fcitx5.addons = with pkgs; [
+     fcitx5-gtk
+   ];
+    type = "fcitx5";
   };
 
   # Enable networking

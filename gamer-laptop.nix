25e7272eb24ddfa2783a8b4a698f7edc05a98970
Almino Melo <almino.melo@gmail.com>
2026-03-14 22:14:05 -0300

    Usa um container para opencode

--- gamer-laptop.nix
+++ gamer-laptop.nix
@@ -69,7 +69,7 @@
   imports = [
     ./almino
     # ./almino/apps/android-studio.nix
-    ./almino/apps/docker.nix
+    ./almino/apps/podman.nix
     ./almino/apps/python.nix
     ./almino/apps/virt-manager.nix
     ./gnome.nix # includes default.nix
